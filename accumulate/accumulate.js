391a9f8d9259aef29c07565f8251d6c17ec40cfe
Michael Herman <hermanmu@gmail.com>
2015-10-19 08:40:48 -0600

    added solution to accumulate

---
+++ accumulate/accumulate.js
@@ -0,0 +1,15 @@
+// function accumulate(arr, fn){
+//   var result = [];
+//   for (var i = 0; i < arr.length; i++){
+//     result.push(fn(arr[i]));
+//   }
+//   return result;
+// }
+
+function accumulate (arr, fn) {
+  return arr.map(function(num){
+    return fun(num);
+  });
+}
+
+module.exports = accumulate;
