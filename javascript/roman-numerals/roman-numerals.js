9b3ad0050b419a69a583a39d6c76a5cec5738c3d
Michael Herman <hermanmu@gmail.com>
2016-01-15 08:50:26 -0700

    added roman numerals

---
+++ javascript/roman-numerals/roman-numerals.js
@@ -0,0 +1,34 @@
+toRoman = function(num) {
+
+  var output = '';
+
+  var arabicToRoman = [
+    {arabic: 1000, roman: 'M'},
+    {arabic: 900, roman: 'CM'},
+    {arabic: 500, roman: 'D'},
+    {arabic: 400, roman: 'CD'},
+    {arabic: 100, roman: 'C'},
+    {arabic: 90, roman: 'XC'},
+    {arabic: 50, roman: 'L'},
+    {arabic: 40, roman: 'XL'},
+    {arabic: 10, roman: 'X'},
+    {arabic: 9, roman: 'IX'},
+    {arabic: 5, roman: 'V'},
+    {arabic: 4, roman: 'IV'},
+    {arabic: 1, roman: 'I'}
+  ];
+
+  for (var i = 0; i < arabicToRoman.length; i++) {
+    var mapping = arabicToRoman[i];
+    console.log(mapping)
+    while (num >= mapping.arabic) {
+      output = output + mapping.roman;
+      num = num - mapping.arabic;
+    }
+  }
+
+  return output;
+
+};
+
+module.exports = toRoman;
